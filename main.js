7f559423d70380d583a0eb4c2c6c627175d600d3
Eugene-301 <ablepiha@duck.com>
2025-06-01 11:04:14 +0300

    First app window

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-console.log("Привет из Electron 👋");
+const { app, BrowserWindow } = require("electron");
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 100,
+    autoHideMenuBar: true,
+    alwaysOnTop: true,
+    resizable: false,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.on("ready", () => {
+  createWindow();
+});
