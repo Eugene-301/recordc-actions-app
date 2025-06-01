c02f18af31a47b03c4e6126dc5040c1a0f75ee9f
Eugene-301 <ablepiha@duck.com>
2025-06-01 11:08:34 +0300

    Added electron forge

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("node:path");
 
 const createWindow = () => {
   const win = new BrowserWindow({
@@ -7,6 +8,9 @@ const createWindow = () => {
     autoHideMenuBar: true,
     alwaysOnTop: true,
     resizable: false,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
   });
 
   win.loadFile("index.html");
